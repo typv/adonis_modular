89b89cfcd16de6ae5be73b22590720586b1973d0
typv <pvtnd1995@gmail.com>
2024-08-06 10:06:32 +0700

    Add department submodule

--- app/modules/department/controllers/department.controller.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpContext } from '@adonisjs/core/http'
-import Department from "#modules/department/models/department";
-
-export default class DepartmentController {
-  async index(ctx: HttpContext) {
-    const res = await Department.find(1)
-    return res
-  }
-}
--- app/modules/department/department.route.ts
+++
@@ -1,4 +0,0 @@
-import router from '@adonisjs/core/services/router'
-const DepartmentController = () => import('#modules/department/controllers/department.controller')
-
-router.get('departments', [DepartmentController, 'index'])
